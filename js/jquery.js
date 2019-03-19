59aa437a6cb81a4cb32d9fdd8bef6f1f60eb7df8
ImraanMeyer <imraan.meyer97@gmail.com>
2019-03-19 14:01:44 +0200

    added lessons

---
+++ js/jquery.js
@@ -0,0 +1,30 @@
+// Jquery is a open source javascript libary that allows us to accomplish awesome thing in our websites. With Jquery you might:
+
+// Add, delete, or modify HTML elements within your page.
+
+// Animate elements on your page
+
+// Send and recieve data from a server via AJAX (asynchronous JS and XML) so your page doesnt have to be reloaded after submitting a form.
+
+// And more 
+
+
+// Basic Syntax
+//  $ function
+
+
+// JavaScript
+
+// let listItems = document.querySelectorAll('li');
+// let i;
+// for (i = 0; i < listItems.length; i++) {
+//     listItems[i].className = 'starred';
+// }
+
+
+// jQuery
+
+// $("li").addClass("starred");
+
+
+// You'll notice a lot of semicolons and brackets in JavaScript, and many $ (dollar signs) in Jquery. For all of these wild symbols, you can accomplish the same things with both JS and jQuery. However, you can often do the same things with less effert and code using the letter
